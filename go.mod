24798c4cbcd28c4c5910411b30ba160acb74c2aa
Russ Cox <rsc@golang.org>
2024-07-08 13:24:38 -0400

    internal/pebble: disk-based storage
    Package pebble implements disk-based storage using
    github.com/cockroachdb/pebble, a production-quality
    LevelDB-like key-value store.
    
    Copied from https://github.com/rsc/gaby/commit/d9fb2ff.
    
    Change-Id: Ia17e78b7311cf915d800c7151356a7b1bf8dc792
    Reviewed-on: https://go-review.googlesource.com/c/oscar/+/597140
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    TryBot-Bypass: Russ Cox <rsc@golang.org>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>

--- go.mod
+++ go.mod
@@ -3,7 +3,36 @@ module golang.org/x/oscar
 go 1.23
 
 require (
+	github.com/cockroachdb/pebble v1.1.1
 	rsc.io/omap v1.2.1-0.20240709133045-40dad5c0c0fb
 	rsc.io/ordered v1.1.0
 	rsc.io/top v1.0.2
+)
+
+require (
+	github.com/DataDog/zstd v1.4.5 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cockroachdb/errors v1.11.3 // indirect
+	github.com/cockroachdb/fifo v0.0.0-20240606204812-0bbfbd93a7ce // indirect
+	github.com/cockroachdb/logtags v0.0.0-20230118201751-21c54148d20b // indirect
+	github.com/cockroachdb/redact v1.1.5 // indirect
+	github.com/cockroachdb/tokenbucket v0.0.0-20230807174530-cc333fc44b06 // indirect
+	github.com/getsentry/sentry-go v0.27.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 )
---
+++ internal/pebble/pebble_test.go
@@ -0,0 +1,86 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package pebble
+
+import (
+	"encoding/binary"
+	"fmt"
+	"math/rand/v2"
+	"path/filepath"
+	"testing"
+
+	"golang.org/x/oscar/internal/storage"
+	"golang.org/x/oscar/internal/testutil"
+)
+
+type testWriter struct{ t *testing.T }
+
+func (w testWriter) Write(b []byte) (int, error) {
+	w.t.Logf("%s", b)
+	return len(b), nil
+}
+
+func TestDB(t *testing.T) {
+	lg := testutil.Slogger(t)
+	dir := t.TempDir()
+	dbname := filepath.Join(dir, "db1")
+
+	db, err := Open(lg, dbname)
+	if err == nil {
+		t.Fatal("Open nonexistent succeeded")
+	}
+
+	db, err = Create(lg, dbname)
+	if err != nil {
+		t.Fatal(err)
+	}
+	db.Close()
+
+	db, err = Create(lg, dbname)
+	if err == nil {
+		t.Fatal("Create already-existing succeeded")
+	}
+
+	db, err = Open(lg, dbname)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	storage.TestDB(t, db)
+
+	if testing.Short() {
+		return
+	}
+
+	// Test that MaybeApply handles very large batch.
+	b := db.Batch()
+	val := make([]byte, 1e6)
+	pcg := rand.NewPCG(1, 2)
+	applied := 0
+	for key := range 500 {
+		for i := 0; i < len(val); i += 8 {
+			binary.BigEndian.PutUint64(val[i:], pcg.Uint64())
+		}
+		binary.BigEndian.PutUint64(val, uint64(key))
+		b.Set([]byte(fmt.Sprint(key)), val)
+		if b.MaybeApply() {
+			if applied++; applied == 2 {
+				break
+			}
+		}
+	}
+	b.Apply()
+
+	for key := range 200 {
+		val, ok := db.Get([]byte(fmt.Sprint(key)))
+		if !ok {
+			t.Fatalf("after batch, missing key %d", key)
+		}
+		if x := binary.BigEndian.Uint64(val); x != uint64(key) {
+			t.Fatalf("Get(%d) = value for %d, want %d", key, x, key)
+		}
+	}
+}
