f38ab38e9a29aedec1af60004c2dc68e56588c49
Jonathan Amsterdam <jba@google.com>
2024-08-15 17:16:40 -0400

    internal/gcp/gcphandler: more information in message
    The GCP log viewer displays only the message of a log entry on the screen.
    To see the attributes requires two clicks, one to open the entry and
    a second to open its "jsonPayload" field.
    
    To mitigate the problem, this handler appends the initial attributes
    to the message, up to a fixed length.
    
    Change-Id: I9aae87ca589f8e66751e9827c1514b2c37db30c0
    Reviewed-on: https://go-review.googlesource.com/c/oscar/+/607136
    Reviewed-by: Tatiana Bradley <tatianabradley@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- internal/gcp/gcphandler/gcphandler.go
+++ internal/gcp/gcphandler/gcphandler.go
@@ -10,9 +10,11 @@
 package gcphandler
 
 import (
+	"context"
 	"io"
 	"log/slog"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -24,17 +26,25 @@ import (
 //   - It replaces the key "traceID" with "logging.googleapis.com/trace.".
 //   - It replaces the value for the "time" key with an RFC3339-formatted string.
 //
+// It also appends some attributes to the message, because the GCP log viewer
+// requires two clicks to get from the main log view to the attributes.
+//
 // [GCP's logging specification]: https://cloud.google.com/logging/docs/agent/logging/configuration#special-fields
 func New(level slog.Leveler) slog.Handler {
 	return newHandler(level, os.Stderr)
 }
 
+// The maximum length of a message after appending attributes.
+// Var for testing.
+var maxMessageLength = 100
+
 // newHandler is for testing.
 func newHandler(level slog.Leveler, w io.Writer) slog.Handler {
-	return slog.NewJSONHandler(w, &slog.HandlerOptions{
+	jh := slog.NewJSONHandler(w, &slog.HandlerOptions{
 		Level:       level,
 		ReplaceAttr: replaceAttr,
 	})
+	return &handler{jh}
 }
 
 // If testTime is non-zero, replaceAttr will use it as the time.
@@ -60,4 +70,42 @@ func replaceAttr(groups []string, a slog.Attr) slog.Attr {
 		a.Key = "logging.googleapis.com/trace"
 	}
 	return a
+}
+
+// handler is a [slog.Handler] that appends some attributes
+// to the message, but otherwise behaves like its underlying
+// handler.
+type handler struct {
+	h slog.Handler
+}
+
+func (h *handler) Enabled(ctx context.Context, lvl slog.Level) bool {
+	return h.h.Enabled(ctx, lvl)
+}
+
+func (h *handler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &handler{h: h.h.WithAttrs(attrs)}
+}
+
+func (h *handler) WithGroup(name string) slog.Handler {
+	return &handler{h: h.h.WithGroup(name)}
+}
+
+// Handle implements [slog.Handler] by appending the first attributes
+// to the message until a limit is reached, then calling the underlying
+// handler.
+func (h *handler) Handle(ctx context.Context, r slog.Record) error {
+	var b strings.Builder
+	b.WriteString(r.Message)
+	r.Attrs(func(a slog.Attr) bool {
+		s := a.String()
+		if b.Len()+1+len(s) > maxMessageLength {
+			return false
+		}
+		b.WriteByte(' ')
+		b.WriteString(s)
+		return true
+	})
+	r.Message = b.String()
+	return h.h.Handle(ctx, r)
 }
--- internal/gcp/gcphandler/gcphandler_test.go
+++ internal/gcp/gcphandler/gcphandler_test.go
@@ -6,24 +6,52 @@ package gcphandler
 
 import (
 	"bytes"
-	"fmt"
+	"context"
 	"log/slog"
 	"testing"
 	"time"
 )
 
 func TestHandler(t *testing.T) {
-	var buf bytes.Buffer
-	l := slog.New(newHandler(slog.LevelInfo, &buf))
-	l = l.With("traceID", "tid")
-	testTime = time.Now()
-	l.Info("hello", slog.String("foo", "bar"), slog.Int("count", 7))
-	got := buf.String()
+	testTime = time.Date(2020, time.August, 20, 1, 2, 3, 0, time.UTC)
+	defer func(old int) { maxMessageLength = old }(maxMessageLength)
+	maxMessageLength = 10
 
-	want := fmt.Sprintf(`{"time":%q,"severity":"INFO","message":"hello","logging.googleapis.com/trace":"tid","foo":"bar","count":7}`,
-		testTime.Format(time.RFC3339))
-	want += "\n"
-	if got != want {
-		t.Errorf("\ngot  %s\nwant %s", got, want)
+	for _, test := range []struct {
+		name    string
+		message string
+		attrs   []slog.Attr
+		want    string
+	}{
+		{
+			"all attrs fit",
+			"hello",
+			[]slog.Attr{slog.Int("c", 7)},
+			`{"time":"2020-08-20T01:02:03Z","severity":"INFO","message":"hello c=7","logging.googleapis.com/trace":"tid","c":7}`,
+		},
+		{
+			"some attrs fit",
+			"hello",
+			[]slog.Attr{slog.Int("c", 7), slog.Int("d", 1)},
+			`{"time":"2020-08-20T01:02:03Z","severity":"INFO","message":"hello c=7","logging.googleapis.com/trace":"tid","c":7,"d":1}`,
+		},
+		{
+			"message too long",
+			"this is a long message",
+			[]slog.Attr{slog.Int("c", 7)},
+			`{"time":"2020-08-20T01:02:03Z","severity":"INFO","message":"this is a long message","logging.googleapis.com/trace":"tid","c":7}`,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			l := slog.New(newHandler(slog.LevelInfo, &buf))
+			l = l.With("traceID", "tid")
+			l.LogAttrs(context.Background(), slog.LevelInfo, test.message, test.attrs...)
+			got := buf.String()
+			want := test.want + "\n"
+			if got != want {
+				t.Errorf("\ngot  %s\nwant %s", got, want)
+			}
+		})
 	}
 }
